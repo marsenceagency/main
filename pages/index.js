4f49e4846882b4f97d87f36cb29d14215f42b198
marsenceagency <hello@marsence.com>
2025-05-28 17:02:08 +0300

    Изменил заголовок

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-gray-900">Marsence Base Ready 🚀</h1>
+      <h1 className="text-4xl font-bold text-gray-900">Marsence Base Baby 🚀</h1>
     </div>
   )
 }
