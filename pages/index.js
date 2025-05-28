ff633a0c5e8513acdf7bcb32244af31d754b20b8
marsenceagency <hello@marsence.com>
2025-05-28 16:54:19 +0300

    Clean initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">Marsence Base Ready 🚀</h1>
+    </div>
+  )
+}
